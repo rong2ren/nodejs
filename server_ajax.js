3a8ec4256069f4c22d11629d2fb5818a5c9bd600
rong2ren <98350815+rong2ren@users.noreply.github.com>
2023-04-19 10:28:47 -0700

    use 2 html and organize files

--- server_ajax.js
+++ server_ajax.js
@@ -12,28 +12,31 @@ app.set('view engine', 'ejs');
 // Define the route for the home page
 
 app.get("/", (req, res) => {
-    res.render("index");
+    //res.render("index");
+    res.sendFile('index.html', { root: __dirname + '/public' });
   });
 
 app.get('/recommend', (req, res) => {
-    //console.log(req.query);
+    /*
     if (req.query.hasOwnProperty('prompt')){
       const userInput = req.query.prompt.trim(); // use the user's text input, for example 'mystery novels'
       res.render('search', { prompt: userInput });
     } else {
       res.render('search');
     }
-    
+    */
+    res.sendFile('search.html', { root: __dirname + '/public' });
   });
 
 // Route for handling search - form submission
 app.post('/search', async (req, res) => {
   try {
-    console.log("got /search post request");
+    
     const userInput = req.body.prompt.trim(); // use the user's text input, for example 'mystery novels'
     if (!userInput) {
         return res.status(400).json({ error: 'userInput is required.' });
-      }
+    }
+    console.log("got AJAX /search POST request: " + userInput);
     const books = await searchBook(userInput); // Call the searchBook function from book_search.js
     // Render the index.ejs template with the books array -> I am no longer render the whole page since i changed it to AJAX
     //res.render('index', { books: books, userInput: userInput });
